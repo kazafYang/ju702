ac9ed6eb50a3bf2f00255f01655d4feef770480f
kazafYang <31726639+kazafYang@users.noreply.github.com>
2017-09-13 21:13:55 +0800

    Create config_inc.php

---
+++ config_inc.php
@@ -0,0 +1,6 @@
+<?php
+$mysql_server_name='172.30.254.220'; //改成自己的mysql数据库服务器
+$mysql_username='root'; //改成自己的mysql数据库用户名
+$mysql_password='y13551960531'; //改成自己的mysql数据库密码
+$mysql_database='ju70'; //改成自己的mysql数据库名
+?>
