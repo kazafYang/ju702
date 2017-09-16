9a03fafde19a266caa05d91130292823cbb7e120
kazafYang <31726639+kazafYang@users.noreply.github.com>
2017-09-16 12:28:14 +0800

    Update a.php

--- a.php
+++ a.php
@@ -11,7 +11,8 @@ $time_length=0;
 
 function machining_price () 
 {
-echo "comming machining_price\n" 
+echo "comming machining_price\n";
+global $stat_date,$time_hour,$time_min,$time_second,$begin_point;
 $url='http://hq.sinajs.cn/list=sz399006'; 
 $html = file_get_contents($url); 
 $pieces = explode(",", $html);
@@ -23,7 +24,7 @@ $time_min=$pieces[1];
 $time_second=$pieces[2];
 } 
 function sleep_time () {
-echo "comming sleep_time";
+echo "comming sleep_time\n";
 while(($time_hour<>9 and $time_min<>30) or $time_hour<>13) {
 machining_price();
 }
@@ -35,7 +36,7 @@ $sql="select min15_point_max from  point_number where min15_point_max=(select ma
 
 
 function nine_count () {
-echo "comming nine_count"
+echo "comming nine_count\n";
 $max=$begin_point;
 $min=$begin_point;
 $time_out_now=($time_hour*3600)+($time_min*60)+$time_second;
@@ -93,13 +94,13 @@ if ($result->num_rows > 0) {
 while(1==1) {    
 machining_price();
     if ($time_hour<9 or ($time_hour==9 and $time_min<30)) {
-      echo "comming mainwhile if";
+      echo "comming mainwhile if--9\n";
     sleep_time();
     }elseif ($time_hour=="11" and $time_min>"30") {
     sleep_time();     
     }elseif ($time_hour=="15" and $time_min>"1") {
-      echo "3point!";
-    exit(0);    
+      echo "3point!\n";
+  //  exit(0);    
     }
     
     if ($time_hour==9 and $time_min==30) {
@@ -113,8 +114,13 @@ $sql = "select stat_time_min from ".$table_name." order by id desc limit 1;";
     $result = $conn->query($sql);
     if ($result<>$time_min){
     $time_out_begin=($time_hour*3600)+($time_min*60)+$time_second+$time_length;
-$sql = "insert into ".$table." (stat_date,stat_time_hour,stat_time_min,begin_point) VALUES ('$stat_date','$time_hour','$time_min','$begin_point')";    
-   $conn->query($sql); 
+   echo "table:$table_name\n"
+   $sql = "insert into $table_name (stat_date,stat_time_hour,stat_time_min,begin_point) VALUES ('$stat_date','$time_hour','$time_min','$begin_point')";    
+   if ($conn->query($sql) === TRUE) {
+    echo "new inser into\n";
+} else {
+    echo "Error: " . $sql . "\n" . $conn->error;
+}
    nine_count();     
     }
 } 
