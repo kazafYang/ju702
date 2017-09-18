7c91033dd9ea6ef8b8246f5edfcac0e25ac2b4dc
kazafYang <31726639+kazafYang@users.noreply.github.com>
2017-09-18 12:47:37 +0800

    Update action.php

--- action.php
+++ action.php
@@ -1,6 +1,6 @@
 <?php
 include 'config_inc.php';
-$showinfo="";
+$showinfo="info";
 $conn = new mysqli($mysql_server_name, $mysql_username, $mysql_password, $mysql_database);
 $code=array("point_number","point_number_zxb","point_number_sz");
 foreach ($code as $value)
@@ -9,7 +9,7 @@ foreach ($code as $value)
     $sql = "SELECT id,begin_point,stat_time_hour FROM $value order by id desc limit 1";
     $result = $conn->query($sql);
     $row = $result->fetch_assoc();
-    $showinfo=$row[id].",".$showinfo;
+    $showinfo=$showinfo.",".$row[id];
 }
 echo $showinfo;
 ?>
