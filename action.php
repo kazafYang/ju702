e6d6c093190a7e265c97945fcbc113ff651f76d9
kazafYang <31726639+kazafYang@users.noreply.github.com>
2017-09-18 14:09:24 +0800

    Update action.php

--- action.php
+++ action.php
@@ -10,7 +10,7 @@ foreach ($code as $value)
     $sql = "SELECT id,min15_k,min15_d,min30_k,min30_d,min60_k,min60_d FROM $value order by id desc limit 1";                                                                  
     $result = $conn->query($sql);                                                                                                                                             
     $row = $result->fetch_assoc();                                                                                                                                            
-    $showinfo=$showinfo.",".$row[id].",".$row[min15_k].",".$row[min15_d].",".$row[min30_k].",".$row[min30_d].",".$row[min60_k].",".$row[min60_d].";";                         
+    $showinfo=$showinfo.",".$row[id].",".$row[min15_k].",".$row[min15_d].",".$row[min30_k].",".$row[min30_d].",".$row[min60_k].",".$row[min60_d];                         
 }                                                                                                                                                                             
 echo $showinfo;                                                                                                                                                               
 ?> 
