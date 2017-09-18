3311f87fd836c067bd30fc1f7cb7f30ca3a6edcf
kazafYang <31726639+kazafYang@users.noreply.github.com>
2017-09-18 11:39:53 +0800

    Create action.php

---
+++ action.php
@@ -0,0 +1,15 @@
+<?php
+include 'config_inc.php';
+$showinfo="";
+$conn = new mysqli($mysql_server_name, $mysql_username, $mysql_password, $mysql_database);
+$code=array("point_number","point_number_zxb","point_number_sz");
+foreach ($code as $value)
+{
+   // echo $value . "<br>";
+    $sql = "SELECT id,begin_point,stat_time_hour FROM $value limit 1";
+    $result = $conn->query($sql);
+    $row = $result->fetch_assoc()
+    $showinfo=$row[id].$showinfo;
+    echo $showinfo;
+}
+?>
