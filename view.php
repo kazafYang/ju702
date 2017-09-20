b3a4c262b62e9de5021b9fc36b9de72565539786
kazafYang <31726639+kazafYang@users.noreply.github.com>
2017-09-20 16:22:17 +0800

    Update view.php

--- view.php
+++ view.php
@@ -8,7 +8,10 @@ foreach ($code as $value)
 {                                                                                                                                                                                                                                                                                                                         
     $sql = "SELECT code,min15_k,min15_d,min15_j,min30_k,min30_d,min30_j,min60_k,min60_d,min60_j FROM $value order by id desc limit 1";                                                                  
     $result = $conn->query($sql);                                                                                                                                             
-    $row = $result->fetch_assoc();                                                                                                                                            
+    $row = $result->fetch_assoc();
+    $row[min15_k]=round($row[min15_k],2);$row[min15_d]=round($row[min15_d],2);$row[min15_j]=round($row[min15_j],2);
+    $row[min30_k]=round($row[min30_k],2);$row[min30_d]=round($row[min30_d],2);$row[min30_j]=round($row[min30_j],2);
+    $row[min60_k]=round($row[min60_k],2);$row[min60_d]=round($row[min60_d],2);$row[min60_j]=round($row[min60_j],2);
     $showinfo=$showinfo.",".$row[code].",15mk:".$row[min15_k].",15md:".$row[min15_d].",15mj：".$row[min15_j].",30mk：".$row[min30_k].",30md：".$row[min30_d].",30mj：".$row[min30_j].",60mK：".$row[min60_k].",60md：".$row[min60_d].",60mj：".$row[min60_j]."<br>";                         
 }                                                                                                                                                                             
 echo $showinfo;                                                                                                                                                               
