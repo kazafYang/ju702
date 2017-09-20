32c2f85b056d9fee12c210d16ec891c1a5e98f4f
kazafYang <31726639+kazafYang@users.noreply.github.com>
2017-09-20 15:14:58 +0800

    Update view.php

--- view.php
+++ view.php
@@ -9,7 +9,7 @@ foreach ($code as $value)
     $sql = "SELECT code,min15_k,min15_d,min15_j,min30_k,min30_d,min30_j,min60_k,min60_d,min60_j FROM $value order by id desc limit 1";                                                                  
     $result = $conn->query($sql);                                                                                                                                             
     $row = $result->fetch_assoc();                                                                                                                                            
-    $showinfo=$showinfo.",".$row[code].",".$row[min15_k].",".$row[min15_d].",".$row[min15_j].",".$row[min30_k].",".$row[min30_d].",".$row[min30_j].",".$row[min60_k].",".$row[min60_d].",".$row[min60_j]."<br>";                         
+    $showinfo=$showinfo.",".$row[code].",15mk:".$row[min15_k].",15md:".$row[min15_d].",15mj：".$row[min15_j].",30mk：".$row[min30_k].",30md：".$row[min30_d].",30mj：".$row[min30_j].",60mK：".$row[min60_k].",60md：".$row[min60_d].",60mj：".$row[min60_j]."<br>";                         
 }                                                                                                                                                                             
 echo $showinfo;                                                                                                                                                               
 ?> 
