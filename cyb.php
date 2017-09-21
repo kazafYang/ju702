d053891555e945bf52deb09745b4b1ed6b16b219
kazafYang <31726639+kazafYang@users.noreply.github.com>
2017-09-21 13:41:27 +0800

    Update cyb.php

--- cyb.php
+++ cyb.php
@@ -1,6 +1,6 @@
 <?php
 include 'config_inc.php';
-
+include 'public_function.php';
 $table_name="point_number";
 $code="159915";
 $begin_point="";
@@ -31,41 +31,6 @@ while(($time_hour==9 and $time_min<30) or ($time_hour<13 and $time_hour>=11) or 
 machining_price();
 }
                        }
-function kdjfifteen () {
-global $begin_point,$conn,$table_name;
-machining_price();
-$sql="select min15_point_max from  $table_name where min15_point_max=(select max(min15_point_max) from  $table_name) order by min15_point_max desc limit 9;";
-$result = $conn->query($sql);
-$row=$result->fetch_assoc();
-$min15_point_max=$row[min15_point_max];
-$sql="select min15_point_min from  $table_name where min15_point_min=(select min(min15_point_min) from  $table_name) order by min15_point_min desc limit 9;";
-$result = $conn->query($sql);
-$row=$result->fetch_assoc();
-$min15_point_min=$row[min15_point_min];
-$sql="select min15_k from $table_name order by id  desc  limit 1,1;"; 
-$result = $conn->query($sql);  
-$row=$result->fetch_assoc();
-$min15_k=$row[min15_k];   
-$sql="select min15_d from $table_name order by id  desc  limit 1,1;";
-$result = $conn->query($sql);  
-$row=$result->fetch_assoc();
-$min15_d=$row[min15_d];
-echo "begin_point:$begin_point~min15_point_max:$min15_point_max~min15_point_min:$min15_point_min~min15_k:$min15_k~min15_d:$min15_d\n";                      
-$rsv=($begin_point-$min15_point_min)/($min15_point_max-$min15_point_min)*100;
-echo "rsv:$rsv";
-$k=2/3*$min15_k+1/3*$rsv;
-$d=2/3*$min15_d+1/3*$k;
-$j=3*$k-2*$d;
-echo "15kdj:$k,$d,$j\n";
-$sql="update $table_name set min15_k='$k' , min15_d='$d' , min15_j='$j' order by id desc limit 1 ; ";
-   if ($conn->query($sql) === TRUE) 
-   {
-    echo "15kdjupdate:update\n";
-     } 
-  else {
-    echo "maxError: " . $sql . $conn->error."\n";
-}  
-}  
 
 function kdjthirty () {
 global $begin_point,$conn,$table_name;
