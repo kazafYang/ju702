f57640e3d817e4976782dd815a1a65d286b5bce2
kazafYang <31726639+kazafYang@users.noreply.github.com>
2017-09-22 17:56:36 +0800

    Update view.php

--- view.php
+++ view.php
@@ -16,33 +16,33 @@ foreach ($code as $value)
     $row[min60_k]=round($row[min60_k],2);$row[min60_d]=round($row[min60_d],2);$row[min60_j]=round($row[min60_j],2);
     
   if (($row[min15_k] >=75 and $row[min15_k] <80) or ($row[min15_d] >=75 and $row[min15_d] <80)){
-    $sellinfo=$sellinfo.$row[code]."75<=15k/d<80#begin 15分大于75#";
+    $sellinfo=$sellinfo.$row[code]."#75<=15k/d<80#begin 15分大于75#";
     } 
     elseif(($row[min15_k] <85 and $row[min15_k] >=80) or ($row[min15_d] >=80 and $row[min15_d] <85)) {
-    $sellinfo=$sellinfo.$row[code]."80<=15k/d<85#begin 15分大于80";  
+    $sellinfo=$sellinfo.$row[code]."#80<=15k/d<85#begin 15分大于80";  
     }
      elseif( $row[min15_k] >=85 or $row[min15_d] >=85) {
-    $sellinfo=$sellinfo.$row[code]."80<=15k/d<85#begin 15分大于85#";
+    $sellinfo=$sellinfo.$row[code]."#85<=15k/d#begin 15分大于85#";
      }  
   
    if (($row[min30_k] >=75 and $row[min30_k] <80) or ($row[min30_d] >=75 and $row[min30_d] <80)){
-    $sellinfo=$sellinfo.$row[code]."=30k/d>=75#begin 30分大于75#";
+    $sellinfo=$sellinfo.$row[code]."#30k/d>=75#begin 30分大于75#";
     } 
     elseif(($row[min30_k] >=80 and $row[min30_k] <85) or ($row[min30_d] >=80 and $row[min30_d] <85)) {
-    $sellinfo=$sellinfo.$row[code]."80<=30k/d<85#begin 30分大于80#";  
+    $sellinfo=$sellinfo.$row[code]."#80<=30k/d<85#begin 30分大于80#";  
     }
     elseif($row[min30_k] >=85  or $row[min30_d] >=85 ){
-    $sellinfo=$sellinfo.$row[code]."85<=30k/d#begin 30分大于85#";  
+    $sellinfo=$sellinfo.$row[code]."#85<=30k/d#begin 30分大于85#";  
     }  
            
    if (($row[min60_k] >=75 and $row[min60_k]<80) or ($row[min60_d] >=75 and $row[min60_d] <80)) {
-    $sellinfo=$sellinfo.$row[code]."75<=60k/d<80#begin 60分大于75#";
+    $sellinfo=$sellinfo.$row[code]."#75<=60k/d<80#begin 60分大于75#";
     }
     elseif(($row[min60_k] >=80 and $row[min60_k] <85) or ($row[min60_d] >=80 and $row[min60_k] <85)){
-    $sellinfo=$sellinfo.$row[code]."80<=60k/d<85#begin 60分大于80#";  
+    $sellinfo=$sellinfo.$row[code]."#80<=60k/d<85#begin 60分大于80#";  
     }
     elseif( $row[min60_k] >=85 or $row[min60_d] >=85 ){
-    $sellinfo=$sellinfo.$row[code]."85<=60k/d#begin 60分大于85#";  
+    $sellinfo=$sellinfo.$row[code]."#85<=60k/d#begin 60分大于85#";  
     } 
            
     if ($row[min15_k] <20 or $row[min15_d] <20 ){
