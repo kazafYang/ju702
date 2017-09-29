d68389a21908eea09ddd4b6eabe929ad65f06ba4
kazafYang <31726639+kazafYang@users.noreply.github.com>
2017-09-29 16:16:33 +0800

    Update batch_data.php

--- tool/batch_data.php
+++ tool/batch_data.php
@@ -1,16 +1,22 @@
+//批量将各个表中的id，进行重新排序；
 <?php
 include 'config_inc.php';
 $conn = new mysqli($mysql_server_name, $mysql_username, $mysql_password, $mysql_database);  
-$table_name="point_number_zxb"; 
-$a=0;    
-for ($i=0;$i<=31;$i++){
-   $sql = "select id from $table_name limit $i,1;";  //max  //find id
+$code=array("point_number","point_number_sz","point_number_sz100","point_number_zxb","point_number_hs","point_number_zq","point_number_jg","point_number_yh");                                                           
+foreach ($code as $value){
+$sql = "select count(*) from $value ;";  //max  //find count data number
+$result=mysqli_query($conn,$sql);
+$row=mysqli_fetch_row($result);
+$count=$row[0];
+$a=0;
+for ($i=0;$i<$count;$i++){
+   $sql = "select id from $value limit $i,1;";  //max  //find id
    $result = $conn->query($sql);
    $row=$result->fetch_assoc();
    $id=$row[id];
     
     $a=$i+1;
-	$sql = "update $table_name set id=$a where id=$id;";  //batch id  
+	$sql = "update $value set id=$a where id=$id;";  //batch id  
 	$conn->query($sql);  
  if ($conn->query($sql) === TRUE) 
    {
@@ -19,5 +25,6 @@ for ($i=0;$i<=31;$i++){
   else {
     echo $i."Error: " . $sql . $conn->error."\n";
 }  	
+}
 }
 ?>
