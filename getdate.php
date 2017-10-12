96578deabc37a933fc12e864d770f3579d131d5d
kazafYang <31726639+kazafYang@users.noreply.github.com>
2017-10-12 20:32:43 +0800

    Update getdate.php

--- getdate.php
+++ getdate.php
@@ -10,7 +10,7 @@ foreach ($code as $value)
     $sql = "SELECT code,min15_k,min15_d,min15_j,min30_k,min30_d,min30_j,min60_k,min60_d,min60_j,kdjday_k,kdjday_d,kdjday_j,buy_one_price,sell_one_price,now_price FROM $value order by id desc limit 1";                                                                  
     $result = $conn->query($sql);                                                                                                                                             
     $row = $result->fetch_assoc();                                                                                                                                            
-    $showinfo=$showinfo.$row[code].",".$row[min15_k].",".$row[min15_d].",".$row[min30_k].",".$row[min30_d].",".$row[min60_k].",".$row[min60_d].",".$row[kdjday_k].",".$row[kdjday_d].",".$row[kdjday_j].",".$row[now_price].",".$row[buy_one_price].",".$row[sell_one_price].";";                         
+    $showinfo=$showinfo.$row[code].",".$row[min15_k].",".$row[min15_d].",".$row[min15_j].",".$row[min30_k].",".$row[min30_d].",".$row[min30_j].",".$row[min60_k].",".$row[min60_d].",".$row[min60_j].",".$row[kdjday_k].",".$row[kdjday_d].",".$row[kdjday_j].",".$row[now_price].",".$row[buy_one_price].",".$row[sell_one_price].";";                         
 }                                                                                                                                                                             
 echo $showinfo;                                                                                                                                                               
 ?> 
