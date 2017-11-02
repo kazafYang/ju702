783794f0743b368f7744d768ea613efc223093c3
kazafYang <31726639+kazafYang@users.noreply.github.com>
2017-11-02 20:36:37 +0800

    Update run.php

--- run.php
+++ run.php
@@ -1,7 +1,18 @@
 <?php
    ignore_user_abort(); // houtai
    set_time_limit(0); // qu xiao shi jian shang xian
-   echo 'start.';
+   echo 'start.'; 
+   for ($i=0;$i<=20;$i++){
    exec("bash together.sh");
-   echo 'end.';
+   sleep(0);
+   $cmd = 'ps aux|grep php -c';
+        $ret = shell_exec("$cmd");
+        $ret = rtrim($ret, "\r\n");
+        echo "test:".$ret;
+        if($ret > 4) {
+        echo "find sussess!";
+        exit(0);
+        }
+  echo 'end.<br>'.$i;
+}
 ?>
