0feb3a5577d8dfa37afbea48c73f38aa097a8807
kazafYang <31726639+kazafYang@users.noreply.github.com>
2017-11-05 21:09:41 +0800

    update

---
+++ page/update.php
@@ -0,0 +1,6 @@
+
+<?php                                                                                                                                                          
+echo $_SERVER["QUERY_STRING"]."<br>";                                                                                                                          
+echo $_GET['number'];                                                                                                                                          
+echo $_GET['code'];                                                                                                                                            
+?>
