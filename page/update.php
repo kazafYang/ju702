0a1a3a2bd7644b0ba7636b86c30fd5b67f316dfa
kazafYang <31726639+kazafYang@users.noreply.github.com>
2017-11-06 11:38:13 +0800

    Update update.php

--- page/update.php
+++ page/update.php
@@ -4,6 +4,6 @@ echo $_SERVER["QUERY_STRING"]."<br>";
 $id=$_GET['id'];                                                                                                                                          
 $code=$_GET['code']; 
 $conn = new mysqli($mysql_server_name, $mysql_username, $mysql_password, $mysql_database);
-$sql = "inser into trade_history ('id','code') values ($id,$code);";
+$sql = "update trade_history set code=$code,buy_number=$buy_number,sell";
 $result = $conn->query($sql);
 ?>
