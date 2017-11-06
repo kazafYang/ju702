5e011d297df06d4e6585919ec77c16b47170103e
kazafYang <31726639+kazafYang@users.noreply.github.com>
2017-11-06 17:01:29 +0800

    Update public_function.php

--- public_function.php
+++ public_function.php
@@ -294,68 +294,87 @@ foreach ($code as $value)
     $row[min60_k]=round($row[min60_k],2);$row[min60_d]=round($row[min60_d],2);$row[min60_j]=round($row[min60_j],2);
     $row[kdjday_k]=round($row[kdjday_k],2);$row[kdjday_d]=round($row[kdjday_d],2);$row[kdjday_j]=round($row[kdjday_j],2);
 //15min  
-  if(($row[min15_k] >= 80 and $row[min15_k] < 85 and ($row[min60_k] >= 50 or $row[min60_d] >= 50)) or ($row[min15_d]>=75 and $row[min15_d]<80 and ($row[min60_k] >= 50 or $row[min60_d] >= 50)))
+  if(($row[min15_k] >= 80 and $row[min15_k] < 85 and ($row[min60_k] >= 40 or $row[min60_d] >= 40)) or ($row[min15_d]>=75 and $row[min15_d]<80 and ($row[min60_k] >= 50 or $row[min60_d] >= 50)))
     {
-    $sql = "select count(*) from $table_name where code=$code and stat_date=$stat_date and stat_time_hour=$stat_time_hour and stat_time_min=$stat_time_min and trade_type=1;";    
+    $sql = "select count(*) from trade_history where code=$code and stat_date='$stat_date' and stat_time_hour='$stat_time_hour' and stat_time_min='$stat_time_min' and trade_type=1;";    
     $result = $conn->query($sql);
     $row=$result->fetch_assoc();     
     if($row[0]==0){
-    $sql = "insert into $value (id,code,stat_time_hour,stat_time_min,status,number,trade_type) values ('','$code','$stat_time_hour','$stat_time_min','$status','$number','$trade_type');";                                                                  
+    $sql = "insert into trade_history (id,code,stat_time_hour,stat_time_min,status,number,trade_type) values ('','$code','$stat_time_hour','$stat_time_min','$status','$number','$trade_type');";                                                                  
     $result = $conn->query($sql); 
        }
     } 
-elseif (($row[min15_k] >= 85 and $row[min15_k] < 90 and ($row[min60_k] >= 50 or $row[min60_d] >= 50)) or ($row[min15_d]>=80 and $row[min15_d]<85 and ($row[min60_k] >= 50 or $row[min60_d] >= 50)))  { 
-   $sellinfo=$sellinfo.$row[code]."--15min:K/D值已经达到:". $row[min15_k]."/".$row[min15_d]."中幅抛筹》》》"; 
-    }
-elseif (($row[min15_k] >= 90 and ($row[min60_k] >= 50 or $row[min60_d] >= 50)) or ($row[min15_d]>=85 and ($row[min60_k] >= 50 or $row[min60_d] >= 50)))  { 
-   $sellinfo=$sellinfo.$row[code]."--15min:K/D值已经达到:". $row[min15_k]."/".$row[min15_d]."清仓抛筹》》》"; 
-     }  
+
 //30min  
    if (($row[min30_k] >=75 and $row[min30_k] <80) or ($row[min30_d] >=75 and $row[min30_d] <80)){
-    $sellinfo=$sellinfo.$row[code]."--30min:K/D值已经达到:". $row[min30_k]."/".$row[min30_d]."30超买~~~~"; 
+    $sql = "select count(*) from trade_history where code=$code and stat_date='$stat_date' and stat_time_hour='$stat_time_hour' and stat_time_min='$stat_time_min' and trade_type=1;";    
+    $result = $conn->query($sql);
+    $row=$result->fetch_assoc();     
+    if($row[0]==0){
+    $sql = "insert into trade_history (id,code,stat_time_hour,stat_time_min,status,number,trade_type) values ('','$code','$stat_time_hour','$stat_time_min','$status','$number','$trade_type');";                                                                  
+    $result = $conn->query($sql); 
+       }
     } 
-    elseif(($row[min30_k] >=80 and $row[min30_k] <85) or ($row[min30_d] >=80 and $row[min30_d] <85)) {
-     $sellinfo=$sellinfo.$row[code]."--30min:K/D值已经达到:". $row[min30_k]."/".$row[min30_d]."30超买~~~~"; 
-    }
-    elseif($row[min30_k] >=85  or $row[min30_d] >=85 ){
-     $sellinfo=$sellinfo.$row[code]."--30min:K/D值已经达到:". $row[min30_k]."/".$row[min30_d]."30超买~~~~";  
-    }  
-           
+   
+ //60分钟          
    if (($row[min60_k] >=75 and $row[min60_k]<80) or ($row[min60_d] >=75 and $row[min60_d] <80)) {
-    $sellinfo=$sellinfo.$row[code]."--60min:K/D值已经达到:". $row[min60_k]."/".$row[min60_d]."60超买---"; 
+    $sql = "select count(*) from trade_history where code=$code and stat_date='$stat_date' and stat_time_hour='$stat_time_hour' and stat_time_min='$stat_time_min' and trade_type=1;";    
+    $result = $conn->query($sql);
+    $row=$result->fetch_assoc();     
+    if($row[0]==0){
+    $sql = "insert into trade_history (id,code,stat_time_hour,stat_time_min,status,number,trade_type) values ('','$code','$stat_time_hour','$stat_time_min','$status','$number','$trade_type');";                                                                  
+    $result = $conn->query($sql); 
+       }
     }
-    elseif(($row[min60_k] >=80 and $row[min60_k] <85) or ($row[min60_d] >=80 and $row[min60_k] <85)){
-    $sellinfo=$sellinfo.$row[code]."--60min:K/D值已经达到:". $row[min60_k]."/".$row[min60_d]."60超买---"; 
-    }
-    elseif( $row[min60_k] >=85 or $row[min60_d] >=85 ){
-    $sellinfo=$sellinfo.$row[code]."--60min:K/D值已经达到:". $row[min60_k]."/".$row[min60_d]."60超买---";   
-    } 
-  
+   
+  //日线
     if (($row[kdjday_k] >=75 and $row[kdjday_k]<80) or ($row[kdjday_d] >=75 and $row[kdjday_d] <80)) {
-    $sellinfo=$sellinfo.$row[code]."--日线:K/D值已经达到:". $row[kdjday_k]."/".$row[kdjday_d]."日线超买关注风险---"; 
+    $sql = "select count(*) from trade_history where code=$code and stat_date='$stat_date' and stat_time_hour='$stat_time_hour' and stat_time_min='$stat_time_min' and trade_type=1;";    
+    $result = $conn->query($sql);
+    $row=$result->fetch_assoc();     
+    if($row[0]==0){
+    $sql = "insert into trade_history (id,code,stat_time_hour,stat_time_min,status,number,trade_type) values ('','$code','$stat_time_hour','$stat_time_min','$status','$number','$trade_type');";                                                                  
+    $result = $conn->query($sql); 
+       }
     }  
-    elseif (($row[kdjday_k] >=80 and $row[kdjday_k] <85) or ($row[kdjday_d] >=80 and $row[kdjday_d] <85)){
-    $sellinfo=$sellinfo.$row[code]."--日线:K/D值已经达到:". $row[kdjday_k]."/".$row[kdjday_d]."日线超买撤离#----"; 
-    }
-    elseif ($row[kdjday_k] >=85 or $row[kdjday_d] >=85){
-    $sellinfo=$sellinfo.$row[code]."--日线:K/D值已经达到:". $row[kdjday_k]."/".$row[kdjday_d]."日线超买清理出局#----"; 
-    }
-  
+   
+  //buy,分钟
     if ($row[min15_k] <20 or $row[min15_d] <20 ){
-    $buyinfo=$buyinfo.$row[code]."--15min:K/D值已经达到:". $row[min15_k]."/".$row[min15_d]."超卖区域，关注》》》";
+    $sql = "select count(*) from trade_history where code=$code and stat_date='$stat_date' and stat_time_hour='$stat_time_hour' and stat_time_min='$stat_time_min' and trade_type=1;";    
+    $result = $conn->query($sql);
+    $row=$result->fetch_assoc();     
+    if($row[0]==0){
+    $sql = "insert into trade_history (id,code,stat_time_hour,stat_time_min,status,number,trade_type) values ('','$code','$stat_time_hour','$stat_time_min','$status','$number','$trade_type');";                                                                  
+    $result = $conn->query($sql); 
+       }
     }  
     if ($row[min30_k] <20 or $row[min30_d] <20 ){
-    $buyinfo=$buyinfo.$row[code]."--30min:K/D值已经达到:". $row[min30_k]."/".$row[min30_d]."超卖区域，试探~~~~~";
+    $sql = "select count(*) from trade_history where code=$code and stat_date='$stat_date' and stat_time_hour='$stat_time_hour' and stat_time_min='$stat_time_min' and trade_type=1;";    
+    $result = $conn->query($sql);
+    $row=$result->fetch_assoc();     
+    if($row[0]==0){
+    $sql = "insert into trade_history (id,code,stat_time_hour,stat_time_min,status,number,trade_type) values ('','$code','$stat_time_hour','$stat_time_min','$status','$number','$trade_type');";                                                                  
+    $result = $conn->query($sql); 
+       }
     }   
     if ($row[min60_k] <20 or $row[min60_d] <20 ){
-    $buyinfo=$buyinfo.$row[code]."--60min:K/D值已经达到:". $row[min60_k]."/".$row[min60_d]."超卖区域，建仓》》》";
+    $sql = "select count(*) from trade_history where code=$code and stat_date='$stat_date' and stat_time_hour='$stat_time_hour' and stat_time_min='$stat_time_min' and trade_type=1;";    
+    $result = $conn->query($sql);
+    $row=$result->fetch_assoc();     
+    if($row[0]==0){
+    $sql = "insert into trade_history (id,code,stat_time_hour,stat_time_min,status,number,trade_type) values ('','$code','$stat_time_hour','$stat_time_min','$status','$number','$trade_type');";                                                                  
+    $result = $conn->query($sql); 
+       }
     }
     if ($row[kdjday_k] <20 or $row[kdjday_d] <20 ){
-    $buyinfo=$buyinfo.$row[code]."--日线K/D值已经达到:". $row[kdjday_k]."/".$row[kdjday_d]."超卖区域，加杠杆----";
+    $sql = "select count(*) from trade_history where code=$code and stat_date='$stat_date' and stat_time_hour='$stat_time_hour' and stat_time_min='$stat_time_min' and trade_type=1;";    
+    $result = $conn->query($sql);
+    $row=$result->fetch_assoc();     
+    if($row[0]==0){
+    $sql = "insert into trade_history (id,code,stat_time_hour,stat_time_min,status,number,trade_type) values ('','$code','$stat_time_hour','$stat_time_min','$status','$number','$trade_type');";                                                                  
+    $result = $conn->query($sql); 
+       }
     }
-    $sellinfo=$sellinfo."<br>";
-    $buyinfo=$buyinfo."<br>";
-    $showinfo=$showinfo.",".$row[code].",15mk:".$row[min15_k].",15md:".$row[min15_d].",15mj：".$row[min15_j].",30mk：".$row[min30_k].",30md：".$row[min30_d].",30mj：".$row[min30_j].",60mK：".$row[min60_k].",60md：".$row[min60_d].",60mj：".$row[min60_j].",dayk：".$row[kdjday_k].",dayd：".$row[kdjday_d].",dayj：".$row[kdjday_j].",cci：".$row[cci]."<br>";                         
 }                
 }
 
