ef06b636732b2c4f5abeb065cdd1ca6af66c80ac
kazafYang <31726639+kazafYang@users.noreply.github.com>
2017-11-08 12:10:33 +0800

    Update trade_history.sql

--- database/trade_history.sql
+++ database/trade_history.sql
@@ -1,8 +1,8 @@
 CREATE TABLE IF not EXISTS `trade_history` (
   id int(8) auto_increment primary key,
   `code` int(8) DEFAULT "0",
+  `number` int(7) DEFAULT "0",
   `stat_date` date DEFAULT NULL,
   `stat_time_hour` int(2) DEFAULT NULL,
   `stat_time_min` int(2) DEFAULT NULL,
-  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
