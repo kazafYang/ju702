fc613025b5365d5855ecfb8bca9a920d5564fcb3
kazafYang <31726639+kazafYang@users.noreply.github.com>
2017-11-08 13:45:58 +0800

    Update syc_date.php

--- tool/syc_date.php
+++ tool/syc_date.php
@@ -6,9 +6,28 @@ $code=array("point_number","point_number_sz","point_number_sz100","point_number_
 $conn = new mysqli($mysql_server_name, $mysql_username, $mysql_password, $mysql_database);  
 foreach ($code as $value)                                                                                                                                                     
 {
-    $sql = "select * from $value order by id asc;";                                                                  
+    //获取需要插入数据总数
+    $sql = "select count(*) from $value;"; 
+    $result=mysqli_query($conn,$sql);
+    $row=mysqli_fetch_row($result);
+    $data_number=$row[0];
+    //循环获取表中的字段值
+    for($i=1;$i<=$data_number;$i++){
+    $sql = "select * from $value where id=$i;";
     $result = $conn->query($sql);     
-     
+    $row=$result->fetch_assoc();
+    $min60_d=$row[min60_d];
+    //开始同步数据    
+    $ch = curl_init('http://www.baidu.com');
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_exec($ch);
+    $status=curl_getinfo($ch, CURLINFO_HTTP_CODE); // 200
+    echo "insert into id:".$status."\n";    
+    curl_close($ch);    
+    }    
 }
 echo $info."end";
 ?>  
+
+
+
