e6bc3bceadf7d685213839aa68bfb3de0a3c6880
kazafYang <31726639+kazafYang@users.noreply.github.com>
2017-11-08 20:16:50 +0800

    Update getdate.php

--- getdate.php
+++ getdate.php
@@ -1,8 +1,9 @@
 <?php                                                                                                                                                                         
-include 'config_inc.php';                                                                                                                                                     
+include 'config_inc.php';  
+$stat_date=date("Y-m-d");
 //select id,code,number,trade_type from trade_history where status=1 order by id asc limit 1; 
 $conn = new mysqli($mysql_server_name, $mysql_username, $mysql_password, $mysql_database);                                                                                                                                                                                                                                      
-    $sql = "select id,code,number,trade_type from trade_history where status=1 order by id asc limit 1;";                                                                  
+    $sql = "select id,code,number,trade_type,trade_buy_price,trade_sell_price from trade_history where status=0 and stat_date=$stat_date order by id asc limit 1;";                                                                  
     $result = $conn->query($sql);                                                                                                                                             
     $row = $result->fetch_assoc();                                                                                                                                            
     echo $row[id].",".$row[code].",".$row[trade_type].",".$row[number];
