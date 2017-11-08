ee21e6add38eda0e27fb1eb2eb85bddcd80228b1
kazafYang <31726639+kazafYang@users.noreply.github.com>
2017-11-08 18:46:43 +0800

    Update update.php

--- page/update.php
+++ page/update.php
@@ -1,12 +1,13 @@
 <?php
 include 'config_inc.php';
 $conn = new mysqli($mysql_server_name, $mysql_username, $mysql_password, $mysql_database);
-echo $_SERVER["QUERY_STRING"]."<br>"; 
+//echo $_SERVER["QUERY_STRING"]."<br>"; 
 $type=$_GET['type'];
 $id=$_GET['id'];                                                                                                                                          
 $code=$_GET['code'];
-$sql=$_GET['sql'];
-echo str_replace("-"," ","Hello world!");
+$sql_update=$_GET['sql'];
+$sql_update=str_replace("-"," ",$sql_update);
+echo $sql;
 //更新trade_history表status
 if (type==1){
 $sql = "select * from trade_history;";
@@ -24,7 +25,9 @@ $result = $conn->query($sql);
 }
 //验证实际插入sql可能存在问题，还是需要组合才行
   elseif(type==4){
-  $sql = "$sql;";
+  echo "comming/n"
+  $sql = "$sql_update;";
+   echo "comming/n".$sql  
   $result = $conn->query($sql);
 }
 ?>
