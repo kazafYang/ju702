449af941837a8d599b3de46677e77741192ff888
kazafYang <31726639+kazafYang@users.noreply.github.com>
2017-11-08 22:36:54 +0800

    Update update.php

--- page/update.php
+++ page/update.php
@@ -5,7 +5,7 @@ $type=$_GET['type'];
 $id=$_GET['id'];                                                                                                                                          
 $code=$_GET['code'];
 $sql_update=$_GET['sql'];
-$sql_update=str_replace("-"," ",$sql_update);
+$sql_update=str_replace("~"," ",$sql_update);
 //更新trade_history表status
 if ($type==1){
 $sql = "select * from trade_history;";
