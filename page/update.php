65effb0578edf40ec3447a5c5973662eb556d95d
kazafYang <31726639+kazafYang@users.noreply.github.com>
2017-11-08 18:08:47 +0800

    Update update.php

--- page/update.php
+++ page/update.php
@@ -8,21 +8,22 @@ $code=$_GET['code'];
 $sql=$_GET['sql'];
 //更新trade_history表status
 if (type==1){
-$sql = "update trade_history set code=$code,buy_number=$buy_number,sell";
+$sql = "select * from trade_history;";
 $result = $conn->query($sql);
   }
   //同步数据库各表数据，让主从数据库数据保持一致
   elseif(type==2){
-  $sql = "update trade_history set code=$code,buy_number=$buy_number,sell";
+  $sql = "select * from trade_history;";
   $result = $conn->query($sql);
 }
   //同步主从数据库trade库交易数据，让数据保持一致
   elseif(type==3){
-  $sql = "update trade_history set code=$code,buy_number=$buy_number,sell";
+  $sql = "select * from trade_history;";
   $result = $conn->query($sql);
 }
+//验证实际插入sql可能存在问题，还是需要组合才行
   elseif(type==4){
-  $sql = "$sql";
+  $sql = "$sql;";
   $result = $conn->query($sql);
 }
 ?>
