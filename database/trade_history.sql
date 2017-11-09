cc87bb9e642fd25aab1eddb1c3d85b849e7e58e6
kazafYang <31726639+kazafYang@users.noreply.github.com>
2017-11-09 17:41:04 +0800

    Update trade_history.sql

--- database/trade_history.sql
+++ database/trade_history.sql
@@ -1,12 +1,15 @@
 CREATE TABLE IF not EXISTS `trade_history` (
-  id int(8)  primary key,
-  `code` int(8) DEFAULT "0",
-  'status' int(2) default NULL,
-  trade_type int(2) default NULL,
-  'trade_buy_price' double default "0",
-  'trade_sell_price' double default "0",
-  `number` int(7) DEFAULT "0",
+  `id` int(8) not NULL ,
+  `code` int(7) DEFAULT "0",
+  'status' int(2) not null default 0 ,
+  `number` int(7) not null default 0 ,
+   'trade_type' int(2) not null default 0 ,
+  'trade_buy_price' double not null default 0,
+  'trade_sell_price' double not null default 0,
   `stat_date` date DEFAULT NULL,
   `stat_time_hour` int(2) DEFAULT NULL,
   `stat_time_min` int(2) DEFAULT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+
+
