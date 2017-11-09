e32d78af96d979e668e2277014f529498fa03b14
kazafYang <31726639+kazafYang@users.noreply.github.com>
2017-11-09 10:54:54 +0800

    Update trade_history.sql

--- database/trade_history.sql
+++ database/trade_history.sql
@@ -1,6 +1,10 @@
 CREATE TABLE IF not EXISTS `trade_history` (
-  id int(8) auto_increment primary key,
+  id int(8)  primary key,
   `code` int(8) DEFAULT "0",
+  'status' int(2) default NULL,
+  trade_type int(2) default NULL,
+  'trade_buy_price' double default "0",
+  'trade_sell_price' double default "0",
   `number` int(7) DEFAULT "0",
   `stat_date` date DEFAULT NULL,
   `stat_time_hour` int(2) DEFAULT NULL,
