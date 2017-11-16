10eb3f32f081b2d9dd50c6b5b9064da148966d47
kazafYang <31726639+kazafYang@users.noreply.github.com>
2017-11-16 15:21:07 +0800

    Update public_function.php

--- public_function.php
+++ public_function.php
@@ -337,7 +337,7 @@
       mysqli_free_result($result);  //释放结果集
       //sell判断
  //判断当前code是否具备卖出资格，后续可以在这里加上开关等限制性的行为；昨日的总数量，就是今日的可卖数量；
-    if($useable_sell_number>1 and $trade_sell_price>$cost_price and $switch=1){   
+    if($useable_sell_number>1 /* and $trade_sell_price>$cost_price */ and $switch=1){   
       if(($trade_min15_k >= 80  and ($trade_min60_k >= 40 or $trade_min60_d >= 40)) or ($trade_min15_d>=75  and ($trade_min60_k >= 40 or $trade_min60_d >= 40)))
       {
      //死叉暂时先不用 if($trade_min15_k >= 80 and $trade_min15_d >= 75 and $trade_min15_j < $trade_min15_k and $trade_min15_j < $trade_min15_d){  
@@ -471,7 +471,7 @@ if ($trade_day_k >=80 or $trade_day_d >=75) {
       }  
     }
     //buy,买入开关限制，当总
-  if($useable_money>1000 and $trade_buy_price<$cost_price and $switch=1){    
+  if($useable_money>1000 /* and $trade_buy_price<$cost_price */ and $switch=1){    
       if ($trade_min15_k <=15 or $trade_min15_d <20 and ($trade_min60_k<70 or $trade_min60_d<70 ) and ($trade_day_k<70 or $trade_day_d<70) ){
       $sql = "select count(*) from trade_history where code='$trade_code' and stat_date='$trade_stat_date' and stat_time_hour='$trade_time_hour' and stat_time_min='$trade_time_min' and trade_type=5;";    
       echo "commingxxxxxxxxxxxxx".$sql;
