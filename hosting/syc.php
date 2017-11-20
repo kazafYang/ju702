6841e041156710e93fa5cde5326f9daa768500c3
kazafYang <31726639+kazafYang@users.noreply.github.com>
2017-11-20 17:38:28 +0800

    Update syc.php

--- hosting/syc.php
+++ hosting/syc.php
@@ -1,17 +1,27 @@
 <?php
 include 'config_inc.php';
 $conn = new mysqli($mysql_server_name, $mysql_username, $mysql_password, $mysql_database);
-    $sql = "select count(*) from trade_bate;";    
+//$code=array("point_number","point_number_sz","point_number_sz100","point_number_zxb","point_number_hs","point_number_zq","point_number_jg","point_number_yh");                                                           
+//foreach ($code as $value){
+//}    
+   //获取当前数据库已存在条数
+    $sql = "select count(*) from $value;";    
       $result=mysqli_query($conn,$sql);
       $row=mysqli_fetch_row($result);
       $trade_id=$row[0]+1;  
      echo $trade_id;
-  $url='http://ju71-n2.193b.starter-ca-central-1.openshiftapps.com/page/update.php?type=3&sql=select~*~from~trade_bate~where~id='.$trade_id; 
+   //获取目标数据库已存在条数
+     $url='http://ju71-n2.193b.starter-ca-central-1.openshiftapps.com/page/update.php?type=3&sql=select~*~from~".$value."~where~id='.$trade_id; 
+     $html = file_get_contents($url); 
+      $str = $html; 
+     $newstr = substr($str,0,strlen($str)-2); 
+   //对比数据差异，开始执行同步
+  $url='http://ju71-n2.193b.starter-ca-central-1.openshiftapps.com/page/update.php?type=3&sql=select~*~from~".$value."~where~id='.$trade_id; 
   $html = file_get_contents($url); 
 $str = $html; 
 $newstr = substr($str,0,strlen($str)-2); 
 echo $newstr; 
-    $sql = "insert into trade_bate values ($newstr);";    
+    $sql = "insert into $value values ($newstr);";    
     $result=mysqli_query($conn,$sql);
 $conn->close();
 ?>
