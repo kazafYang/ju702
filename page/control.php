99b2f0e02a0fc216d6fe7aa445106180b4affaf2
kazafYang <31726639+kazafYang@users.noreply.github.com>
2017-11-23 20:37:36 +0800

    Update control.php

--- page/control.php
+++ page/control.php
@@ -2,7 +2,7 @@
         $cmd = 'ps aux|grep php -c';                                                                                                                                          
         $ret = shell_exec("$cmd");                                                                                                                                            
         $ret = rtrim($ret, "\r\n");                                                                                                                                           
-        echo "test:".$ret;                                                                                                                                                    
+     //   echo "test:".$ret;                                                                                                                                                    
         if($ret > 4) {                                                                                                                                                        
         echo "sussess!";                                                                                                                                                      
         }                                                                                                                                                                     
