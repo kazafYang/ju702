b0cd8ebb939e09b64aa75cefbc35840ad2518b5b
kazafYang <31726639+kazafYang@users.noreply.github.com>
2017-11-29 12:15:52 +0800

    Update config_inc.php

--- config_inc.php
+++ config_inc.php
@@ -1,5 +1,5 @@
 <?php
-$mysql_server_name='172.30.254.220'; //改成自己的mysql数据库服务器,n2:10.128.10.14
+$mysql_server_name='10.128.20.59'; //改成自己的mysql数据库服务器,n2:10.128.10.14
 $mysql_username='root'; //改成自己的mysql数据库用户名
 $mysql_password='y13551960531'; //改成自己的mysql数据库密码
 $mysql_database='ju70'; //改成自己的mysql数据库名
