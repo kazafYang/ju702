f04b25ae3f2096e2469b3de941b3ae476c9e6de2
kazafYang <31726639+kazafYang@users.noreply.github.com>
2018-07-18 07:53:58 +0800

    add 日志

--- together.sh
+++ together.sh
@@ -1,12 +1,12 @@
 #!/bin/base
 ps -ef|grep php|grep -v grep|awk '{print $2}'|xargs kill -9 #kill all find php delete php distinct
-php cyb.php &
+php zxb.php &
 php hs.php &
 php jg.php &
 php s100.php &
 php sz50.php &
 php yh.php &
 php zq.php &
-php zxb.php
+php cyb.php >>logs/test.log   
 ps -ef|grep php|grep -v grep|awk '{print $2}'|xargs kill -9 #kill all find php
 exit 0
