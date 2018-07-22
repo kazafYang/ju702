88786bcd9d0bd534302bf8e2eacebf2c3456b45e
kazafYang <31726639+kazafYang@users.noreply.github.com>
2018-07-23 07:06:19 +0800

    add 7238

--- index.php
+++ index.php
@@ -61,7 +61,7 @@ if ($conn->connect_error) {
 	while($row=mysqli_fetch_array($result)){
         $trade_price=machining_price($code=$row[code]);
 	//手动干预	
-	$FUrl="http://www.ju70.com/page/update.php?type=4&sql=insert~into~trade_history~(id,code,status,trade_type,number,trade_buy_price,trade_sell_price,stat_date,stat_time_hour,stat_time_min,trade_number,cut_price,connecttion_id)~values~(".trade_history($conn).",$row[code],0,11,$row[number],$trade_price,$trade_price,'$stat_date','$time_hour','$time_min',0,0,$row[id]);";
+	$FUrl="http://www.ju70.com/page/update.php?type=4&sql=insert~into~trade_history~(id,code,status,trade_type,number,trade_buy_price,trade_sell_price,stat_date,stat_time_hour,stat_time_min,trade_number,cut_price,connecttion_id)~values~(".trade_history($conn).",$row[code],0,11,$row[number],$trade_price,$trade_price,'".$stat_date."','".$time_hour."','".$time_min."',0,0,$row[id]);";
         //echo $FUrl;
 	$FUrl_a="<a href='$FUrl'>立即操作</a>";	
 	echo '<tr><td>'.$row[id].'</td><td>'.$row[code].'</td><td>'.$row[status].'</td><td>'.$row[vifi_status].'</td><td>'.$row[trade_type].'</td><td>'.$row[number].'</td><td>'.$row[trade_buy_price].'</td><td>'.$row[trade_sell_price].'</td><td>'.$row[cut_price].'</td><td>'.$row[history_make_money].'</td><td>'."$FUrl_a".'</td><td>'.$row[stat_date].'</td></tr>';
