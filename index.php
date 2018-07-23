9766da13dba669db229344a059eed665e7bd8723
kazafYang <31726639+kazafYang@users.noreply.github.com>
2018-07-23 12:54:55 +0800

    Update index.php

--- index.php
+++ index.php
@@ -64,10 +64,10 @@ if ($conn->connect_error) {
         $trade_price=machining_price($code=$row[code]);
 	//手动干预
 	$trade_id=trade_history($conn);	
-	$FUrl="http://www.ju70.com/page/update.php?type=5&sql=insert~into~trade_history~(id,code,status,trade_type,number,trade_buy_price,trade_sell_price,stat_date,stat_time_hour,stat_time_min,trade_number,cut_price,connecttion_id)~values~(".$trade_id.",$row[code],0,11,$row[number],$trade_price,$trade_price,$stat_date,$time_hour,$time_min,0,0,$row[id]);";
+	$FUrl="http://www.ju70.com/page/update.php?type=5&sql=insert~into~trade_history~(id,code,status,trade_type,number,trade_buy_price,trade_sell_price,stat_date,stat_time_hour,stat_time_min,trade_number,cut_price,connecttion_id)~values~(".$trade_id.",$row[code],0,11,$row[number],$trade_price,$trade_price,$stat_date,$time_hour,$time_min,0,0,$row[id])";
         $trade_update="&trade_update=update~trade_history~set~status=1,vifi_status=1,connecttion_id=$trade_id~where~id=$row[id];";
-	//echo $FUrl;
-	$FUrl_a="<a href='$FUrl'.$trade_update>立即操作</a>";	
+        //$trade_sql=$FUrl.$trade_update;
+	$FUrl_a="<a href=".$FUrl.$trade_update."'>立即操作</a>";	
 	echo '<tr><td>'.$row[id].'</td><td>'.$row[code].'</td><td>'.$row[status].'</td><td>'.$row[vifi_status].'</td><td>'.$row[trade_type].'</td><td>'.$row[number].'</td><td>'.$row[trade_buy_price].'</td><td>'.$row[trade_sell_price].'</td><td>'.$row[cut_price].'</td><td>'.$row[history_make_money].'</td><td>'."$FUrl_a".'</td><td>'.$row[stat_date].'</td></tr>';
 	}
 
