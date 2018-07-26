230dcd30d8f59ff7065e3a98aa5d1b2f9da43274
kazafYang <31726639+kazafYang@users.noreply.github.com>
2018-07-26 20:35:05 +0800

    add 添加各标的日志

--- together.sh
+++ together.sh
@@ -1,12 +1,12 @@
 #!/bin/base
 ps -ef|grep php|grep -v grep|awk '{print $2}'|xargs kill -9 #kill all find php delete php distinct
-php zxb.php &
-php hs.php &
-php jg.php &
-php s100.php &
-php sz50.php &
-php yh.php &
-php zq.php &
-php cyb.php >>logs/test.log   
+php zxb.php & >>logs/test_zxb.log
+php hs.php & >>logs/test_hs.log
+php jg.php & >>logs/test_jg.log
+php s100.php & >>logs/test_sz100.log
+php sz50.php & >>logs/test_sz50.log
+php yh.php & >>logs/test_yh.log
+php zq.php & >>logs/test_zq.log
+php cyb.php >>logs/test_cyb.log   
 ps -ef|grep php|grep -v grep|awk '{print $2}'|xargs kill -9 #kill all find php
 exit 0
