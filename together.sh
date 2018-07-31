893961640488e9bd8a828ebcc7a7d6c3b7e6b214
kazafYang <31726639+kazafYang@users.noreply.github.com>
2018-07-31 12:26:13 +0800

    修复一个写入日志的bug

--- together.sh
+++ together.sh
@@ -1,13 +1,13 @@
 #!/bin/base
 ps -ef|grep php|grep -v grep|awk '{print $2}'|xargs kill -9 #kill all find php delete php distinct
 d1=`date +%Y%m%d`
-php zxb.php & >>logs/test_zxb$d1.log
-php hs.php & >>logs/test_hs$d1.log
-php jg.php & >>logs/test_jg$d1.log
-php s100.php & >>logs/test_sz100$d1.log
-php sz50.php & >>logs/test_sz50$d1.log
-php yh.php & >>logs/test_yh$d1.log
-php zq.php & >>logs/test_zq$d1.log
+php zxb.php  >>logs/test_zxb$d1.log &
+php hs.php  >>logs/test_hs$d1.log &
+php jg.php  >>logs/test_jg$d1.log &
+php s100.php  >>logs/test_sz100$d1.log &
+php sz50.php  >>logs/test_sz50$d1.log &
+php yh.php  >>logs/test_yh$d1.log &
+php zq.php  >>logs/test_zq$d1.log &
 php cyb.php >>logs/test_cyb$d1.log   
 ps -ef|grep php|grep -v grep|awk '{print $2}'|xargs kill -9 #kill all find php
 exit 0
