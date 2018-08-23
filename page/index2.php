9f9e651a9d4eb6557374e3c6e2d36a0aa136f687
kazafYang <31726639+kazafYang@users.noreply.github.com>
2018-08-23 21:32:26 +0800

    增加盈亏做T展示，后续还需要增加

--- page/index2.php
+++ page/index2.php
@@ -8,6 +8,7 @@
   <option value ="saab">一周订单</option>
   <option value="opel">已核销订单</option>
   <option value="audi">未核销订单</option>
+  <option value="maket">做T数据</option>
 </select>
   <input type="submit" value="Submit" />
 </form>
@@ -55,6 +56,11 @@ if ($conn->connect_error) {
                $sql = "select * from trade_history where  trade_type>20 and status=1 and vifi_status=0 order by id desc;"; 
                $sql_count = "select sum(history_make_money) from trade_history where trade_type>20;";		      
 	       break;
+	       case "maket":
+	       $stat_date=date("Y-m-d",strtotime("-90 day"));
+               $sql = "select * from hive_number where code=159915 order by stat_date asc;"; 
+               //$sql_count = "select sum(history_make_money) from trade_history where trade_type>20;";		      
+	       break;
 	       default:
                $sql = "select * from trade_history order by id desc;"; 
 	       $sql_count = "select sum(history_make_money) from trade_history where trade_type>20;";		      
@@ -64,6 +70,7 @@ if ($conn->connect_error) {
     //echo $sql;	
     //查询已交易完成订单
     $result = $conn->query($sql);
+	if($select_date != "maket"){
 	echo '<table border="1"><tr><th>id</th><th>code</th><th>status</th><th>vifi_status</th><th>trade_type</th><th>number</th><th>trade_buy_price</th><th>trade_sell_price</th><th>cut_price</th><th>history_make_money</th><th>connecttion_id</th><th>stat_date</th><</tr>';
 	while($row=mysqli_fetch_array($result)){
 	echo '<tr><td>'.$row[id].'</td><td>'.$row[code].'</td><td>'.$row[status].'</td><td>'.$row[vifi_status].'</td><td>'.$row[trade_type].'</td><td>'.$row[number].'</td><td>'.$row[trade_buy_price].'</td><td>'.$row[trade_sell_price].'</td><td>'.$row[cut_price].'</td><td>'.$row[history_make_money].'</td><td>'.$row[connecttion_id].'</td><td>'.$row[stat_date].'</td></tr>';
@@ -75,6 +82,16 @@ if ($conn->connect_error) {
 	$row=mysqli_fetch_array($result);
 	echo '<table border="1"><tr><th>id</th></tr>';
 	echo '<tr><td>'.$row[0].'</td></tr>';
+	}
+	else{
+	echo '<table border="1"><th>code</th><th>total_money</th><th>stat_date</th><th>make_money</th><th>stat_date</th><th>trade_buy_price</th><th>trade_sell_price</th><th>cut_price</th><th>history_make_money</th><th>connecttion_id</th><th>stat_date</th><</tr>';
+	$old_total_money=0;
+	while($row=mysqli_fetch_array($result)){
+	$make_money=$row[total_money]-$old_make_money;
+	$old_make_money=$row[total_money];
+	echo '<tr><td>'.$row[code].'</td><td>'.$row[total_money].'</td><td>'.$make_money.'</td><td>'.$row[stat_date].'</td></tr>';
+	}
+	}
 	mysqli_free_result($result);  //释放结果集
 	$conn->close();
 ?>
