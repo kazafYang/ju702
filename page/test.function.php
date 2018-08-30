b9f42eefafdd374994ed55b611fe8d163e421ebb
kazafYang <31726639+kazafYang@users.noreply.github.com>
2018-08-30 09:56:45 +0800

    Update test.function.php

--- page/test.function.php
+++ page/test.function.php
@@ -67,9 +67,14 @@ class Decide {
   	
   function day_kdj($par){
   //获取今日kdj数据
+  //$row=result_select("select count(*) from day_point where stat_date like '2018-08-27%';");
+  $row=result_select("select stat_date from $table_name where stat_date<'$stat_date' and stat_time_hour=14 and stat_time_min=45 and kdjday_k>=$kdjday_k-5 and kdjday_k<=$kdjday_k+5 order by id desc limit 1;"); 	  
+  while($row=mysqli_fetch_array($result)){
+  
+  $row=result_select("select * from day_point where stat_date>='$row[stat_date]' and stat_date<=$stat_date'+5");	  
+  }	  
    $row=result_select("select stat_date from $table_name where stat_date<'$stat_date' and stat_time_hour=14 and stat_time_min=45 and kdjday_k>=$kdjday_k-5 and kdjday_k<=$kdjday_k+5 order by id desc limit 1;");
    
-   $row=result_select("select count(*) from day_point where stat_date like '2018-08-27%';");
   }
   
   function getTitle(){
