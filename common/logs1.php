407a3884859717f16a835f3f754e2cd5647c6f71
kazafYang <31726639+kazafYang@users.noreply.github.com>
2018-09-06 17:43:51 +0800

    Update logs1.php

--- common/logs1.php
+++ common/logs1.php
@@ -2,7 +2,7 @@
     class logs{	
         var $time;	
          	
-        function log_work($text){	
+        public function log_work($text){	
 			date_default_timezone_set('prc');	
 			$time=date('Y-m-d H:i:s');	
 			echo $time." - ".$text."\n";	
