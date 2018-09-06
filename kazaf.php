6d22670f70ffeb4eb92be98615b5ea7d2d04f5ba
kazafYang <31726639+kazafYang@users.noreply.github.com>
2018-09-06 18:57:47 +0800

    Update kazaf.php

--- kazaf.php
+++ kazaf.php
@@ -8,5 +8,5 @@ $runoob = new MachiningPrice();
 $data=$runoob->get_machining_price();
 echo $data['time_hour'];
 $db=new db();
-echo $db->get_id("point_number");
+echo ($db->get_id("point_number"));
 ?>
