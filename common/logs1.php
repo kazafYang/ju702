f7b2880a472ee58a3d61cade678b9110fb6ac805
kazafYang <31726639+kazafYang@users.noreply.github.com>
2018-09-06 17:05:18 +0800

    Create logs1.php

---
+++ common/logs1.php
@@ -0,0 +1,11 @@
+<?php	
+    class logs{	
+        var $time;	
+         	
+        function log_work($text){	
+			date_default_timezone_set('prc');	
+			$time=date('Y-m-d H:i:s');	
+			echo $time." - ".$text."\n";	
+        }	
+    }	
+?>
