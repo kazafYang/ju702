43f5ab5f64ac14b19dca950ecb6f62ac74d784f6
kazafYang <31726639+kazafYang@users.noreply.github.com>
2018-09-06 17:45:40 +0800

    增加初始化

--- common/db.php
+++ common/db.php
@@ -1,9 +1,14 @@
 <?php
 class db{
-  var $conn=new mysqli($mysql_server_name, $mysql_username, $mysql_password, $mysql_database);
-  var $log=new logs();
-  
-  function result_select($sql){
+  var $conn;
+  var $log;
+  public function __construct($conn,$log)
+    {
+        $this->conn = new mysqli($mysql_server_name, $mysql_username, $mysql_password, $mysql_database);
+        $this->log = new logs();
+    }
+	
+   public function get_select($sql){
 	$log -> log_work("result_select函数执行的sql：$sql\n");
 	$result = $conn->query($sql);
 	$row=mysqli_fetch_array($result);
@@ -11,7 +16,7 @@ class db{
 	mysqli_free_result($result);  //释放结果集
 }
 
-function table_id ($table_name) {
+    public function get_id($table_name) {
 	$sql = "select id from $table_name order by id desc limit 1;"; //where status=0 and stat_date='$stat_date'
 	$log -> log_work("查询下一个插入数据的id，sql语句为：$sql\n");
 	$result = $conn->query($sql);
