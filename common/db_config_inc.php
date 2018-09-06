65fdd47a89f6b4737273df6c0e30d8c0cf8d8c62
kazafYang <31726639+kazafYang@users.noreply.github.com>
2018-09-06 19:58:02 +0800

    Update db_config_inc.php

--- common/db_config_inc.php
+++ common/db_config_inc.php
@@ -2,12 +2,12 @@
 class DB_Config_Inc{
     
     public function get_db_config(){
-        $data['mysql_server_name'] = 'mysql.ju70.svc';
-        $data['mysql_username'] = 'root';
-        $data['mysql_password'] = 'y13551960531';
-        $data['mysql_database'] = 'ju70';
-        $conn = new mysqli($mysql_server_name, $mysql_username, $mysql_password, $mysql_database);  
-        return $conn;
+    $mysql_server_name='mysql.ju70.svc'; //改成自己的mysql数据库服务器,n2:10.128.10.14
+    $mysql_username='root'; //改成自己的mysql数据库用户名
+    $mysql_password='y13551960531'; //改成自己的mysql数据库密码
+    $mysql_database='ju70'; //改成自己的mysql数据库名
+    $conn = new mysqli($mysql_server_name, $mysql_username, $mysql_password, $mysql_database);  
+    return $conn;
     }
 }
 ?>
