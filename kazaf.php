eb31ee8e7ffec88d03779c3304bd5b04b268de13
kazafYang <31726639+kazafYang@users.noreply.github.com>
2018-09-07 06:58:31 +0800

    Update kazaf.php

--- kazaf.php
+++ kazaf.php
@@ -4,10 +4,25 @@ include 'common/machining_price.php';
 include 'common/db_config_inc.php';
 include 'common/db.php';
 
+class Runner{
+  public $config = array();
+  
+  public function get_config(){
+  $config['table_name']="point_number";
+  $config['code']="159915";  
+  return $config;  
+  }
+  
+}
+$aa=new Runner();
+$aa->get_config();
+//echo $aa->get_config()['table_name'];
+//echo $bb['table_name']."\n";
+
 $runoob = new MachiningPrice();
 $data=$runoob->get_machining_price();
 echo $data['time_hour'];
 $db=new db();
-echo ($db->get_id("point_number"));
+echo ($db->get_id($aa->get_config()['table_name']));
 echo "结束\n";
 ?>
