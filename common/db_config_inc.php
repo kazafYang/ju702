5c72686d20f89a979e70cf8f24b138a607d39fff
kazafYang <31726639+kazafYang@users.noreply.github.com>
2018-09-06 19:13:09 +0800

    Update db_config_inc.php

--- common/db_config_inc.php
+++ common/db_config_inc.php
@@ -7,7 +7,8 @@ class DB_Config_Inc{
         $data['mysql_username'] = 'root';
         $data['mysql_password'] = 'y13551960531';
         $data['mysql_database'] = 'ju70';
-        return $data;
+        $conn = new mysqli($mysql_server_name, $mysql_username, $mysql_password, $mysql_database);  
+        return $conn;
     }
 }
 ?>
