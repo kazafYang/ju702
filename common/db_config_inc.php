e331c5d49483f9a5d45e08327864deaba77941fd
kazafYang <31726639+kazafYang@users.noreply.github.com>
2018-09-06 18:17:59 +0800

    Create db_config_inc.php

---
+++ common/db_config_inc.php
@@ -0,0 +1,13 @@
+<?php
+class DB_Config_Inc{
+    var $data = array();
+    
+    public function(){
+        $data['mysql_server_name'] = 'mysql.ju70.svc';
+        $data['mysql_username'] = 'root';
+        $data['mysql_password'] = 'y13551960531';
+        $data['mysql_database'] = 'ju70';
+        return $data;
+    }
+}
+?>
