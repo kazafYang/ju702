533f1082d2fbe345c93c37ecefa1b06146a43149
kazafYang <31726639+kazafYang@users.noreply.github.com>
2018-09-06 15:43:08 +0800

    Update kazaf.php

--- kazaf.php
+++ kazaf.php
@@ -1,5 +1,7 @@
-include 'logs.php';
-include 'machining_price.php';
+<?php
+include 'common/logs.php';
+include 'common/machining_price.php';
 
 $runoob = new MachiningPrice();
 echo $runoob->get_machining_price();
+?>
