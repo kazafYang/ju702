4b436dbd48dff0aa3444ad869a5b6b5b516b9ab4
kazafYang <31726639+kazafYang@users.noreply.github.com>
2018-09-12 22:06:16 +0800

    Update analyse.php

--- common/analyse.php
+++ common/analyse.php
@@ -132,26 +132,26 @@ function set_analyse () {
        $this->log -> log_work("comming switch-buy~~~~~day-kdj-min15~~$trade_min15_k~$trade_min15_d~"."\n"); 
        $trade_type=21; 
        $trade_bite=$type21;	    
-       buy_action($trade_type);
+       buy_action($trade_type,$trade_bite);
       }  
     if ($trade_min30_k <=15 or $trade_min30_d <=20){
        $this->log -> log_work("comming switch-buy~~~~~day-kdj-min30~$trade_min30_k~$trade_min30_d~~"."\n"); 
        $trade_type=22; 
        $trade_bite=$type22;	    
-       buy_action($trade_type);
+       buy_action($trade_type,$trade_bite);
       }   
     if ($trade_min60_k <=15 or $trade_min60_d <=20){ 
        $this->log -> log_work("comming switch-buy~~~~~day-kdj-min60~$trade_min60_k~$trade_min60_d~~"."\n"); 
        $trade_type=23; 
        $trade_bite=$type23;	    
-       buy_action($trade_type);
+       buy_action($trade_type,$trade_bite);
       }
 	  //120日线超卖
       if ($trade_min120_k <=15 or $trade_min120_d <=20){
        $this->log -> log_work("comming switch-buy~~~~~day-kdj-min120~$trade_min120_k~$trade_min120_d~~"."\n"); 
        $trade_type=24; 
        $trade_bite=$type24;	    
-       buy_action($trade_type);
+       buy_action($trade_type,$trade_bite);
       }  	  
   }    //日线超卖完成
   $this->log -> log_work("回转sell开始\n");	  
@@ -236,7 +236,7 @@ $this->log -> log_work("回转buy开始\n");
        $this->log -> log_work("coming huizhuan_buy:$loser_price~$buy_switched~$useable_money~$trade_day_k~$trade_day_k\n");
        $trade_type=26; 
        $trade_bite=$type26;	    
-       buy_action($trade_type);  
+       buy_action($trade_type,$trade_bite);  
        $sql = "update trade_history set status=2 where id=$loser_price_id;";                                                                  
        $this->db->set_update($sql);    
       }
