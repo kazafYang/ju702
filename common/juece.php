a009ec1cd13de801845d94d697a67891d4bb106a
kazafYang <31726639+kazafYang@users.noreply.github.com>
2018-10-04 20:53:19 +0800

    Update juece.php

--- common/juece.php
+++ common/juece.php
@@ -1,28 +1,29 @@
 <?php
 class Juece{
 
-function __construct() {
-	$this->log = new logs();
+	function __construct() {
+		$this->log = new logs();
 
-}
-function get_buy($trade_type){
-  $this->log -> log_work("comming get_buy\n");
-  
-}
+	}
+	function get_buy($trade_type){
+	  $this->log -> log_work("comming get_buy\n");
+		
+	}
 
-function get_sell($trade_type){
-  $this->log -> log_work("comming get_sell\n");
+	function get_sell($trade_type){
+	  $this->log -> log_work("comming get_sell\n");
 
-}
+	}
 
-function get_returnbuy($trade_type){
-  $this->log -> log_work("comming get_returnbuy\n");
+	function get_returnbuy($trade_type){
+	  $this->log -> log_work("comming get_returnbuy\n");
 
-}
+	}
 
-function get_returnsell($trade_type){
-  $this->log -> log_work("comming get_returnsell\n");
+	function get_returnsell($trade_type){
+	  $this->log -> log_work("comming get_returnsell\n");
 
-}
+	}
+	
 }
 ?>
